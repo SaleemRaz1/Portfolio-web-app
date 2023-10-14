320428a2a534546e7c88d94d9e9ba2772ad4fe7d
Saleem11 <1salimraz@gmail.com>
2023-10-14 21:58:12 +0500

    portfolio

---
+++ porfolio/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Category, Portfolio
+
+
+def portfolio_page(request):
+    return render(request, 'portfolio/portfolio.html')
+
+def portfolio_details(request, portfolio_id):
+    item = Portfolio.objects.get(id=portfolio_id)
+    context = {
+        'item': item
+    }
+    return render(request, 'portfolio/portfolio-details.html', context)
+
